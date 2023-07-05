ad66d65c8c81a5088aa571e5e98010470c92de06
Richard Neumann <r.neumann@homeinfo.de>
2023-07-05 12:10:21 +0200

    Implement configuration application

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,18 @@
+use configparser::ini::Ini;
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
 
+const HTML5DS_CONFIG: &str = "/etc/html5ds.conf";
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
-    ConfigurationFailed,
+    ConfigurationFailed(String),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::ConfigurationFailed => write!(f, "configuration failed"),
+            Self::ConfigurationFailed(_) => write!(f, "configuration failed"),
         }
     }
 }
@@ -31,6 +34,14 @@ impl Config {
     /// # Errors
     /// Returns an [`digsigctl::config::Error`] if the configuration could not be applied
     pub fn apply(&self) -> Result<(), Error> {
-        todo!("Where do we store the URL on the system?")
+        let mut html5ds_config = Ini::new();
+        html5ds_config
+            .load(HTML5DS_CONFIG)
+            .map_err(Error::ConfigurationFailed)?;
+        html5ds_config.set("application", "url", Some(self.url.clone()));
+        html5ds_config
+            .write(HTML5DS_CONFIG)
+            .map_err(|error| Error::ConfigurationFailed(error.to_string()))?;
+        Ok(())
     }
 }
