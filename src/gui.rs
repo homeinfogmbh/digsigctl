1e2a424999eeb634670ecde10eeefba5752cb3e3
Richard Neumann <r.neumann@homeinfo.de>
2023-07-06 18:09:08 +0200

    Refactor GUI

--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,6 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
-use gtk4 as gtk;
 use gtk4::glib::{clone, timeout_future_seconds, MainContext, Priority};
-use gtk4::{Align, Button};
+use gtk4::prelude::*;
+use gtk4::{Align, Application, ApplicationWindow, Button};
 use std::fs::read_to_string;
 use std::sync::mpsc::{sync_channel, SyncSender};
 use std::thread;
@@ -35,31 +33,34 @@ fn show_hostname(hostname: String) {
             .title(hostname.trim())
             .build();
 
-        add_close_button(&window);
-        make_window_close_channel(window.clone());
+        let button = Button::with_label("Ok");
+        button.connect_clicked(|button| {
+            if let Some(parent) = button.parent() {
+                if let Ok(window) = parent.downcast::<ApplicationWindow>() {
+                    window.close();
+                }
+            }
+        });
+
+        window.set_child(Some(&button));
+        set_timeout(window.clone(), TIMEOUT_SECONDS);
+
         window.show();
     });
 
     application.run_with_args::<&str>(&[]);
 }
 
-fn add_close_button(window: &ApplicationWindow) {
-    let button = Button::with_label("Ok");
-    let button_window = window.clone();
-    button.connect_clicked(move |_| button_window.close());
-    window.set_child(Some(&button));
-}
-
-fn make_window_close_channel(window: ApplicationWindow) {
+fn set_timeout(window: ApplicationWindow, seconds: u32) {
     let (sender, receiver) = MainContext::channel(Priority::default());
 
-    MainContext::default().spawn_local(clone!(@strong sender => async move {
-        timeout_future_seconds(TIMEOUT_SECONDS).await;
-        sender.send(window).expect("Could not send through channel");
-    }));
-
-    receiver.attach(None, move |window| {
+    receiver.attach(None, move |window: ApplicationWindow| {
         window.close();
         Continue(true)
     });
+
+    MainContext::default().spawn_local(clone!(@strong sender => async move {
+        timeout_future_seconds(seconds).await;
+        sender.send(window).expect("Could not send through channel");
+    }));
 }
