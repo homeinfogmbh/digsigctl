f15b2e3268664f399135348348d2237545e69696
Richard Neumann <r.neumann@homeinfo.de>
2023-07-06 14:06:36 +0200

    Remove broken visual identification
    https://stackoverflow.com/questions/76627546/show-gtk-gui-on-http-request-via-rocket

--- src/rpc/identify.rs
+++ src/rpc/identify.rs
@@ -1,31 +1,6 @@
-use crate::create;
+use crate::rpc::beep::beep;
 use crate::rpc::Result as RpcResult;
-use beep_evdev::Melody;
-use gtk4::prelude::ApplicationExtManual;
-use std::fs::read_to_string;
-
-const HOSTNAME: &str = "/etc/hostname";
 
 pub fn identify() -> RpcResult {
-    let acoustic_result = Melody::default().play();
-    let visual_result = identify_visually();
-
-    match (acoustic_result, visual_result) {
-        (Err(acoustic_error), Err(visual_error)) => RpcResult::Error(
-            [
-                acoustic_error.to_string().into(),
-                visual_error.to_string().into(),
-            ]
-            .as_slice()
-            .into(),
-        ),
-        (Err(acoustic_error), _) => RpcResult::Error(acoustic_error.to_string().into()),
-        (_, Err(visual_error)) => RpcResult::Error(visual_error.to_string().into()),
-        _ => RpcResult::Success(None),
-    }
-}
-
-fn identify_visually() -> Result<(), std::io::Error> {
-    create(read_to_string(HOSTNAME)?).run_with_args::<&str>(&[]);
-    Ok(())
+    beep(None)
 }
