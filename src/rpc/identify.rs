3a31ad24385403e554b0a71a836c530970a20825
Richard Neumann <mail@richard-neumann.de>
2023-07-07 08:31:23 +0200

    Use xmessage to display hostname

--- src/rpc/identify.rs
+++ src/rpc/identify.rs
@@ -1,6 +1,37 @@
 use crate::rpc::beep::beep;
 use crate::rpc::Result;
+use std::fs::read_to_string;
+use subprocess::{Popen, PopenConfig, Redirection};
+
+const HOSTNAME: &str = "/etc/hostname";
+const XMESSAGE_TIMEOUT: u32 = 15;
 
 pub fn identify() -> Result {
-    beep(None)
+    let beep_result = beep(None);
+    let xmessage_result = read_to_string(HOSTNAME).map_or_else(
+        |error| Result::Error(error.to_string().into()),
+        |hostname| {
+            xmessage(hostname.as_str(), XMESSAGE_TIMEOUT).map_or_else(
+                |error| Result::Error(error.to_string().into()),
+                |_| Result::Success(None),
+            )
+        },
+    );
+    beep_result + xmessage_result
+}
+
+fn xmessage(text: &str, timeout: u32) -> subprocess::Result<Popen> {
+    Popen::create(
+        &[
+            "xmessage",
+            "-center",
+            "-timeout",
+            timeout.to_string().as_str(),
+            text,
+        ],
+        PopenConfig {
+            stdout: Redirection::None,
+            ..Default::default()
+        },
+    )
 }
