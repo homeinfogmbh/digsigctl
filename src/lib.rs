bfd8b461e89936afdfa32dfb791cd975f06dd0c9
Richard Neumann <mail@richard-neumann.de>
2023-07-07 08:19:44 +0200

    Remove Gtk GUI

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod config;
-mod gui;
 mod net;
 mod rpc;
 mod sysinfo;
 
 pub use config::Config;
-pub use gui::spawn;
 pub use net::discover_address;
 pub use rpc::{Command, Result};
 pub use sysinfo::SystemInformation;
