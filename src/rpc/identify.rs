b7cbc6786b256770458f66d814890354c88b3552
Richard Neumann <mail@richard-neumann.de>
2023-08-18 01:08:19 +0200

    Move os-dependent stuff into sub-module

--- src/rpc/identify.rs
+++ src/rpc/identify.rs
@@ -1,14 +1,9 @@
 use crate::rpc::beep::beep;
 use crate::rpc::Result;
-use std::fs::read_to_string;
-use subprocess::{Popen, PopenConfig, Redirection};
-
-const ETC_HOSTNAME: &str = "/etc/hostname";
-const XMESSAGE_TIMEOUT_SEC: u8 = 15;
 
 #[cfg(target_family = "unix")]
 pub fn identify() -> Result {
-    beep(None) + display_hostname()
+    beep(None) + unix::display_hostname()
 }
 
 #[cfg(target_family = "windows")]
@@ -16,29 +11,38 @@ pub fn identify() -> Result {
     beep(None)
 }
 
-fn display_hostname() -> Result {
-    read_to_string(ETC_HOSTNAME).map_or_else(
-        |error| Result::Error(error.to_string().into()),
-        |hostname| {
-            xmessage(hostname.trim(), XMESSAGE_TIMEOUT_SEC)
-                .map_or_else(std::convert::Into::into, |_| Result::Success(Box::new(())))
-        },
-    )
-}
+#[cfg(target_family = "unix")]
+mod unix {
+    use std::fs::read_to_string;
+    use subprocess::{Popen, PopenConfig, Redirection};
 
-fn xmessage(text: &str, timeout: u8) -> subprocess::Result<Popen> {
-    Popen::create(
-        &[
-            "xmessage",
-            "-center",
-            "-timeout",
-            timeout.to_string().as_str(),
-            text,
-        ],
-        PopenConfig {
-            stdout: Redirection::None,
-            detached: true,
-            ..Default::default()
-        },
-    )
+    const ETC_HOSTNAME: &str = "/etc/hostname";
+    const XMESSAGE_TIMEOUT_SEC: u8 = 15;
+
+    fn display_hostname() -> Result {
+        read_to_string(ETC_HOSTNAME).map_or_else(
+            |error| Result::Error(error.to_string().into()),
+            |hostname| {
+                xmessage(hostname.trim(), XMESSAGE_TIMEOUT_SEC)
+                    .map_or_else(std::convert::Into::into, |_| Result::Success(Box::new(())))
+            },
+        )
+    }
+
+    fn xmessage(text: &str, timeout: u8) -> subprocess::Result<Popen> {
+        Popen::create(
+            &[
+                "xmessage",
+                "-center",
+                "-timeout",
+                timeout.to_string().as_str(),
+                text,
+            ],
+            PopenConfig {
+                stdout: Redirection::None,
+                detached: true,
+                ..Default::default()
+            },
+        )
+    }
 }
