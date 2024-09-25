a216f25c45b123eef98d30d0db099b2d5e107b29
Richard Neumann <mail@richard-neumann.de>
2024-09-25 20:32:10 +0200

    Add docs to sensors()

--- src/sysinfo/sensors.rs
+++ src/sysinfo/sensors.rs
@@ -4,6 +4,12 @@ use std::process::{Command, Stdio};
 const SENSORS: &str = "/usr/bin/sensors";
 const JSON: &str = "-j";
 
+/// Collect information about local sensors.
+///
+/// # Errors
+///
+/// This function returns an [`anyhow::Error`] if the values could not be read
+/// from the subcommand or could not be parsed from its output.
 pub fn sensors() -> anyhow::Result<serde_json::Value> {
     Ok(serde_json::from_slice(
         Command::new(SENSORS)
