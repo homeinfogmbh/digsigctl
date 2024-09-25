84aeaf0745b42706b59ba9bf631b8b9de004951d
Richard Neumann <mail@richard-neumann.de>
2024-09-25 20:45:32 +0200

    Add docs to try_from_io.rs

--- src/try_from_io.rs
+++ src/try_from_io.rs
@@ -1,7 +1,9 @@
+/// Helper trait to read types from a vector of bytes.
 pub trait TryFromIo: Sized {
     fn try_from_io(bytes: Vec<u8>) -> std::io::Result<Self>;
 }
 
+/// Implementation of the above helper trait to allow to read strings from a bytes buffer.
 impl TryFromIo for String {
     fn try_from_io(bytes: Vec<u8>) -> std::io::Result<Self> {
         Self::from_utf8(bytes).map_err(|error| {
