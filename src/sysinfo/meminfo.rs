53e97e318670d38023cd57f30559e3fad2525f01
Richard Neumann <mail@richard-neumann.de>
2024-09-29 00:34:23 +0200

    Refactor meminfo() into one expression

--- src/sysinfo/meminfo.rs
+++ src/sysinfo/meminfo.rs
@@ -8,24 +8,25 @@ const PROC_MEMINFO: &str = "/proc/meminfo";
 /// # Errors
 ///
 /// This function will return an [`std::io::Error`] if `/proc/meminfo` could not be read.
-pub fn meminfo() -> Result<HashMap<String, usize>, std::io::Error> {
-    Ok(read_to_string(PROC_MEMINFO)?
-        .lines()
-        .filter_map(|line| line.split_once(':'))
-        .filter_map(|(key, value)| {
-            value
-                .trim()
-                .split_once(' ')
-                .map_or(value.trim().parse::<usize>().ok(), |(value, unit)| {
-                    value.trim().parse::<usize>().ok().and_then(|value| {
-                        match unit.trim() {
-                            "kB" => Some(1024),
-                            _ => None,
-                        }
-                        .map(|factor| factor * value)
+pub fn meminfo() -> std::io::Result<HashMap<String, usize>> {
+    read_to_string(PROC_MEMINFO).map(|text| {
+        text.lines()
+            .filter_map(|line| line.split_once(':'))
+            .filter_map(|(key, value)| {
+                value
+                    .trim()
+                    .split_once(' ')
+                    .map_or(value.trim().parse::<usize>().ok(), |(value, unit)| {
+                        value.trim().parse::<usize>().ok().and_then(|value| {
+                            match unit.trim() {
+                                "kB" => Some(1024),
+                                _ => None,
+                            }
+                            .map(|factor| factor * value)
+                        })
                     })
-                })
-                .map(|value| (key.trim().to_string(), value))
-        })
-        .collect())
+                    .map(|value| (key.trim().to_string(), value))
+            })
+            .collect()
+    })
 }
