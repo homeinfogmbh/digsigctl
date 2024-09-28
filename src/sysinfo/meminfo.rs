83267eb709bdbb6884008bc11d50f8c410b6b3a6
Richard Neumann <mail@richard-neumann.de>
2024-09-29 00:38:24 +0200

    Split meminfo() into helper functions

--- src/sysinfo/meminfo.rs
+++ src/sysinfo/meminfo.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use std::path::Path;
 
 const PROC_MEMINFO: &str = "/proc/meminfo";
 
@@ -9,24 +10,31 @@ const PROC_MEMINFO: &str = "/proc/meminfo";
 ///
 /// This function will return an [`std::io::Error`] if `/proc/meminfo` could not be read.
 pub fn meminfo() -> std::io::Result<HashMap<String, usize>> {
-    read_to_string(PROC_MEMINFO).map(|text| {
-        text.lines()
-            .filter_map(|line| line.split_once(':'))
-            .filter_map(|(key, value)| {
-                value
-                    .trim()
-                    .split_once(' ')
-                    .map_or(value.trim().parse::<usize>().ok(), |(value, unit)| {
-                        value.trim().parse::<usize>().ok().and_then(|value| {
-                            match unit.trim() {
-                                "kB" => Some(1024),
-                                _ => None,
-                            }
-                            .map(|factor| factor * value)
-                        })
+    meminfo_from_file(PROC_MEMINFO)
+}
+
+fn meminfo_from_file(filename: impl AsRef<Path>) -> std::io::Result<HashMap<String, usize>> {
+    read_to_string(filename.as_ref()).map(meminfo_from_text)
+}
+
+fn meminfo_from_text(text: impl AsRef<str>) -> HashMap<String, usize> {
+    text.as_ref()
+        .lines()
+        .filter_map(|line| line.split_once(':'))
+        .filter_map(|(key, value)| {
+            value
+                .trim()
+                .split_once(' ')
+                .map_or(value.trim().parse::<usize>().ok(), |(value, unit)| {
+                    value.trim().parse::<usize>().ok().and_then(|value| {
+                        match unit.trim() {
+                            "kB" => Some(1024),
+                            _ => None,
+                        }
+                        .map(|factor| factor * value)
                     })
-                    .map(|value| (key.trim().to_string(), value))
-            })
-            .collect()
-    })
+                })
+                .map(|value| (key.trim().to_string(), value))
+        })
+        .collect()
 }
